1cfbd2af10b5960809282b944c292903a3e4435a
Legonois <wesley.baldwin@outlook.com>
2022-06-16 15:50:16 -0500

    Revert "Added Bootstrap Navbar"
    This reverts commit f1447e99c95531e8b67038817797d387084534f6.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import Header from './Components/Header';
-
 
 function App() {
   const value = 'IKR GAMERS';
-
-  return ( 
-  <div>
-        <Header/>
-  </div>
-  );
+  return <div>Hello {value}</div>;
 }
-
-
 
 export default App;
