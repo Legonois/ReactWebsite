7126b54fb8c108273a11fabe74c3ec02220a4480
Legonois <wesley.baldwin@outlook.com>
2022-06-16 15:50:11 -0500

    Revert "Removed bootstrap stuff"
    This reverts commit def17c44fba54968fa5f164ef25232da2de1754e.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,9 +2,9 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-        <nav /*className='navbar navbar-expand-lg navbar-dark bg-primary'*/>
-            <div /*className="container-fluid container"*/>
-                {<a /*className='navbar-brand'*/> Test Navbar</a> }
+        <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
+            <div className="container-fluid">
+                <a className='navbar-brand'> Test Navbar</a>
             </div>
         </nav>
     </header>
