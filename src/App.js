f1447e99c95531e8b67038817797d387084534f6
Legonois <wesley.baldwin@outlook.com>
2022-06-16 14:57:59 -0500

    Added Bootstrap Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Header from './Components/Header';
+
 
 function App() {
   const value = 'IKR GAMERS';
-  return <div>Hello {value}</div>;
+
+  return ( 
+  <div>
+        <Header/>
+  </div>
+  );
 }
+
+
 
 export default App;
